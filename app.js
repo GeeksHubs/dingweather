a690cae56fac89d9bde5884f6849e7024f4f00d7
Marios Isaakidis <misaakidis@yahoo.gr>
2014-11-13 04:35:20 +0100

    On search submit, update page with new city's current weather; Use openweathermap-lib v0.2.0

--- app.js
+++ app.js
@@ -1,6 +1,26 @@
-window.addEventListener("load", function() {
-  var weather = WeatherLib.getCurrentCoordJSON(34, -5);
+var updateCity = function(city) {
+  var weather = WeatherLib.getCurrentCityJSON(city);
+  if (weather === undefined) {
+    alert("The city" + city + " was not found");
+    return;
+  }
   
   var cityLabel = document.getElementById("#city");
+  var maxTempLabel = document.getElementById("#max_temp");
+  var minTempLabel = document.getElementById("#min_temp");
+  var descLabel = document.getElementById("#desc");
   cityLabel.innerHTML = weather.location + ', ' + weather.country;
+  maxTempLabel.innerHTML = weather.temp_max;
+  minTempLabel.innerHTML = weather.temp_min;
+  descLabel.innerHTML = weather.long_desc;
+};
+
+window.addEventListener("load", function() {
+  var searchCityForm = document.forms["#searchCityForm"];
+  searchCityForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+    updateCity(searchCityForm.elements["city"].value);
+  });
+  
+  updateCity("Valencia");
 });
